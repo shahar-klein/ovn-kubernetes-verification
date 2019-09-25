daf3c52251071188b21a826938700b777eaa1fab
root <root@node1.cluster.local>
2019-09-25 19:03:32 +0000

    add north south test

--- ping1.sh
+++ ping1.sh
@@ -21,6 +21,7 @@ kubectl -v=6 create -f $D/ping1.yaml
 kubectl wait --for=condition=Ready pod/node2-pod --timeout=10s || (echo "ERROR starting pod/node2-pod" ; exit 1)
 kubectl wait --for=condition=Ready pod/node3-pod --timeout=10s || (echo "ERROR starting pod/node3-pod" ; exit 1)
 
+title "ping east-west"
 IP_NODE2=$(kubectl get pod node2-pod --template={{.status.podIP}})
 kubectl exec -it node3-pod -- ping -W 2 -c 2 $IP_NODE2
 RV=$?
@@ -28,6 +29,18 @@ if [ $RV -ne 0 ] ; then
 	err $TEST
 	exit 1
 fi
+
+
+title "ping north-south"
+kubectl exec -it node3-pod -- ping -W 38 -c 3 8.8.8.8
+kubectl exec -it node3-pod -- ping -W 38 -c 3 8.8.8.8
+
+RV=$?
+if [ $RV -ne 0 ] ; then
+        err $TEST
+        exit 1
+fi
+
 
 success $TEST
 
